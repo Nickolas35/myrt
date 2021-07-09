ccf77bab16964bcfc22c2f36151a186456f24b9a
Nickolas35 <86787140+Nickolas35@users.noreply.github.com>
2021-07-09 15:45:09 +0300

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
+    effects.confetti.startScreenEffect()
+    music.baDing.play()
+    game.showLongText("97gtiyfiky", DialogLayout.Bottom)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    game.showLongText("lauyfxhf", DialogLayout.Bottom)
+})
 let mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite)
