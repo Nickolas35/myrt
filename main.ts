68522425e314043e6e42eb54f865b5e1462b4e2a
Nickolas35 <86787140+Nickolas35@users.noreply.github.com>
2021-07-09 18:51:34 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
-    music.powerUp.play()
-    game.showLongText("97gtiyfiky", DialogLayout.Bottom)
+    music.jumpUp.play()
+    game.showLongText("YOU WIN!!!", DialogLayout.Bottom)
     effects.confetti.startScreenEffect()
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
-    music.powerDown.play()
-    game.showLongText("lauyfxhf", DialogLayout.Bottom)
+    music.jumpDown.play()
+    game.showLongText("You are not rihgt try again!", DialogLayout.Bottom)
 })
 let mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
-scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite)
 tiles.setTilemap(tilemap`level1`)
+forever(function () {
+    scene.cameraFollowSprite(mySprite)
+})
